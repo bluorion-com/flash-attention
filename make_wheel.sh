5c253a08d7f0922544e3c62526b3259a11412187
Fabian Guera <fabian.guera@gmail.com>
2024-08-14 11:02:59 +0000

    Fix target in script.

--- make_wheel.sh
+++ make_wheel.sh
@@ -1,2 +1,2 @@
-bazel run third_party/flash-attn::build_wheel
+bazel run third_party/flash-attn:build_wheel
 cp bazel-bin/third_party/flash-attn/build_wheel.runfiles/_main/dist/*.whl /mnt/nfs/home/gmi/prebuilt_wheels/
