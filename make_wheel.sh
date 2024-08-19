91560c4a194c104f93171c819c602edf7d8f624c
Fabian Guera <fabian.guera@gmail.com>
2024-08-19 11:32:27 +0000

    Check out/update submodules by default.

--- make_wheel.sh
+++ make_wheel.sh
@@ -1,3 +1,5 @@
+./update_submodules.sh
+
 bazel run third_party/flash-attention:build_wheel
 
 cp bazel-bin/third_party/flash-attention/build_wheel.runfiles/_main/dist/*.whl /mnt/nfs/home/gmi/prebuilt_wheels/
