f3a3e38bec80c4f833f69b51998a1dee3d166d48
Fabian Guera <fabian.guera@gmail.com>
2025-01-16 09:06:06 +0000

    Use absolute path in build target.

--- make_wheel.sh
+++ make_wheel.sh
@@ -6,9 +6,9 @@ fi
 
 ./update_submodules.sh
 
-bazel run third_party/flash-attention:build_wheel
+bazel run //third_party/flash-attention:build_wheel
 
 cp bazel-bin/third_party/flash-attention/build_wheel.runfiles/_main/dist/*.whl /mnt/nfs/home/gmi/prebuilt_wheels/
 
-ls /mnt/nfs/home/gmi/prebuilt_wheels/flash_*
+ls /mnt/nfs/home/gmi/prebuilt_wheels/flash_* -la
 
